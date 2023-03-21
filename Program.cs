7412a2af372e54d5d0e858a17f0577a8581de161
Mert Temizer <mert10temizer@gmail.com>
2023-03-21 21:24:44 +0300

    Data Type Short

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// DATA TYPE : Short -> -32768  32767
+
+short first = 4500;
+short second = -3740;
+
+Console.WriteLine(first);
+Console.WriteLine(second);
+
+short maxDeger = short.MaxValue;
+short minDeger = short.MinValue;
+
+Console.WriteLine(maxDeger);
+Console.WriteLine(minDeger);
+
+
